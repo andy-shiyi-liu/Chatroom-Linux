8276a5c819a9540d4a84695ad8d33f8451e1988f
andyliu <andyliu.92@outlook.com>
2022-05-03 20:10:10 +0800

    CHECKPOINT: before merge from qzj

--- server/include/at.c
+++ server/include/at.c
@@ -10,38 +10,38 @@ extern struct userinfo users[MAXCAPACITY];
 void fine_name(char *buf, int uid)
 {
     char destname[MAXLINE];
-    for(int i = 0; i < MAXLINE; i++)
+    for (int i = 0; i < MAXLINE; i++)
     {
         destname[i] = '\0';
     }
-    for(int i = 1; buf[i] != ' ' && buf[i] != '@'; i++)  
-    {   
-        destname[i-1] = buf[i];
+    for (int i = 1; buf[i] != ' ' && buf[i] != '@'; i++)
+    {
+        destname[i - 1] = buf[i];
         k = i;
         if (buf[i] == '\0')
             break;
     }
 
-    while(buf[k] != '\0' && buf[k] != '@')
+    while (buf[k] != '\0' && buf[k] != '@')
     {
         k++;
     }
-        
-    if (buf[k] == '@')
-        fine_name(buf+k, uid);    
 
-    if(strcmp(destname, ALL)==0)
+    if (buf[k] == '@')
+        fine_name(buf + k, uid);
+
+    if (strcmp(destname, ALL) == 0)
     {
-        for(int i = 0; i < MAXCAPACITY; i++)
+        for (int i = 0; i < MAXCAPACITY; i++)
         {
             users[i].at = 1;
         }
     }
     else
     {
-        for(int i = 0; i < MAXCAPACITY; i++)
+        for (int i = 0; i < MAXCAPACITY; i++)
         {
-            if(strcmp(destname, users[i].name) == 0)
+            if (strcmp(destname, users[i].name) == 0)
             {
                 users[i].at = 1;
             }
@@ -53,11 +53,11 @@ void at(char *buf, const int uid, char *BrdMsg)
 {
 
     printf("receive message: ");
-	printf("%s(uid=%d):%s\n", users[uid].name, users[uid].id, buf);
+    printf("%s(uid=%d):%s\n", users[uid].name, users[uid].id, buf);
     fine_name(buf, uid);
- 
+
     for (int i = 0; i < MAXCAPACITY; i++)
-	{   
+    {
         if (i == uid)
             continue;
         else if (users[i].at)
@@ -68,11 +68,10 @@ void at(char *buf, const int uid, char *BrdMsg)
         {
             sprintf(BrdMsg, "%s:\t\033[37m%s\033", users[uid].name, buf);
         }
-			write(users[i].fd, BrdMsg, MAXLINE);
-	}
-    for(int i = 0; i < MAXCAPACITY; i++)
+        write(users[i].fd, BrdMsg, MAXLINE);
+    }
+    for (int i = 0; i < MAXCAPACITY; i++)
     {
         users[i].at = 0;
     }
 }
-
